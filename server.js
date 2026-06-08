d434485b595c1db8893388dc14c84184ed01115e
刘福临 <flagliu0@gmail.com>
2026-06-08 10:33:37 +0800

    增加发布商品时可上传一张图片功能，增大body-parser限制为10mb

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const DB_PATH = path.join(__dirname, 'data.db');
